fa9992137ea7f3c063399f17e4314d9cd9ae0c03
NazarkoNaN <nazar.ivanyuk@gmail.com>
2023-10-25 00:47:21 -0700

    Django model instance to dictionary

--- beckend/api/views.py
+++ beckend/api/views.py
@@ -1,4 +1,23 @@
-from django.http import JsonResponse
+import json
+from django.forms.models import model_to_dict
+from django.http import JsonResponse, HttpResponse
+
+from products.models import Product
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your first Django API response!! "})
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    # reguest -> HttpRequest -> Django
+    print(request.GET) #url query params
+    print(request.POST)
+    body = request.body #byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    data['params'] = dict(request.POST)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    print(data)
+    return JsonResponse(data)
