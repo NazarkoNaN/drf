4eb1e93841790dfd7495a87a69feff104dd0a766
NazarkoNaN <nazar.ivanyuk@gmail.com>
2023-10-25 20:44:47 -0700

    Generics Retrive API View

--- beckend/products/serializer.py
+++ beckend/products/serializer.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
-    discount = serializers.SerializerMethodField(read_only=True)
+    my_discount = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
         fields = [
@@ -11,8 +11,12 @@ class ProductSerializer(serializers.ModelSerializer):
             'content',
             'price',
             'sale_price',
-            'discount',
+            'my_discount',
         ]
 
-    def get_discount(self, obj):
+    def get_my_discount(self, obj):
+        if not hasattr(obj,'id'):
+            return None
+        if not isinstance(obj, Product):
+            return None
         return obj.get_discount()
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 #endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" #"http://127.0.0.1:8000/"
 
-get_response = requests.get(endpoint, json={"product_id": 123})
+get_response = requests.post(endpoint, json={"title": "Hello world!!!"})
 #print(get_response.headers)
 #print(get_response.text)
 #print(get_response.status_code)
